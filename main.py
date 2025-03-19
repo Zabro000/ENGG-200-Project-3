296ce4ffaffde5432316f8ef2a494540cdde159a
brianoh-123 <brianoh1684@gmail.com>
2025-03-18 20:13:59 -0600

    while

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 # Find the average distances for the front, right, and left sensors
 from hcsr04 import HCSR04 # Must have this library saved on Pico to work
 from time import sleep
+from machine import Pin
+from time import sleep
 
 # Front Sensor
 # Initialize sensor with trigger and echo pins
 sensor = HCSR04(trigger_pin=21, echo_pin=20)
+#Sensor right
+sensor_r = HCSR04(trigger_pin=19, echo_pin=18)
+#Sensor Left
+sensor_l = HCSR04(trigger_pin=17, echo_pin=16)
 
 average_front = []
+average_right = []
+average_left = []
+
+# Reed swich on pin 0 using internal pull down resistor, other wire of switch connects to 3.3V
+reed_switch = Pin(0, Pin.IN, Pin.PULL_DOWN)
+led = Pin('LED', Pin.OUT)
+reed_average = []
+
+from machine import Pin
+from time import sleep
+
+# Initialize input with internal pull down resistor on pin 10
+# When using internal pull down resistor on Pico, pin will have logic level 1 (3.3 V) when button pushed and 0 when released
+# FOr internal pull up resistor, pin will have logic level 0 when button pushed and 1 (3.3 V) when released
+#ssss
+button = Pin(10, Pin.IN, Pin.PULL_DOWN)
+
+# Initialize on board LED
+led = Pin('LED', Pin.OUT)
+but = []
+
 while True:
     try:
         distance = sensor.distance_cm()
@@ -23,13 +50,8 @@ while True:
         print('ERROR getting distance:', ex)
         break
 
-#Sensor right
-sensor_r = HCSR04(trigger_pin=19, echo_pin=18)
-
-average_right = []
-while True:
     try:
-        distance_r = sensor.distance_cm()
+        distance_r = sensor_r.distance_cm()
         print('Distance:', distance_r, 'cm')
         sleep(0.1) # sensor doesn't work well without delay
         average_right.append(distance_r)
@@ -42,13 +64,8 @@ while True:
         print('ERROR getting distance:', ex)
         break
 
-#Sensor Left
-sensor_r = HCSR04(trigger_pin=17, echo_pin=16)
-
-average_left = []
-while True:
     try:
-        distance_l = sensor.distance_cm()
+        distance_l = sensor_l.distance_cm()
         print('Distance:', distance_l, 'cm')
         sleep(0.1) # sensor doesn't work well without delay
         average_left.append(distance_l)
@@ -60,19 +77,9 @@ while True:
     except OSError as ex:
         print('ERROR getting distance:', ex)
         break
-    
-from machine import Pin
-from time import sleep
 
-# Reed swich on pin 0 using internal pull down resistor, other wire of switch connects to 3.3V
-reed_switch = Pin(0, Pin.IN, Pin.PULL_UP)
-led = Pin('LED', Pin.OUT)
-
-reed_average = []
-while True:
     if reed_switch.value() == 1:  # Check if the magnet is near
         led.value(1)# Turn on the LED
-
         
     else:
         led.value(0)  # Turn off the LED
@@ -85,45 +92,31 @@ while True:
         if sum(reed_average) == 5:
             print("Payload detected")
             magnet = 1
-        
+        else:
+            print("Payload not Detected")
+            magnet = 0
         reed_average = []
-
     
     sleep(0.1)  # Short delay
 
-from machine import Pin
-from time import sleep
-
-# Initialize input with internal pull down resistor on pin 10
-# When using internal pull down resistor on Pico, pin will have logic level 1 (3.3 V) when button pushed and 0 when released
-# FOr internal pull up resistor, pin will have logic level 0 when button pushed and 1 (3.3 V) when released
-#ssss
-button = Pin(10, Pin.IN, Pin.PULL_DOWN)
-
-# Initialize on board LED
-led = Pin('LED', Pin.OUT)
-
-while True:
-    
     # if button pushed turn on LED
     if button.value() == 1:
         print("Button Pressed!")
         led.on()
     else:
+        print("Not Pressed")
         led.off()
+    
+    but.append(button.value())
+    if len(but) == 5:
+        if sum(but) == 5:
+            print("Payload on")
+            payload_stay = 1
+        but = []
     
     sleep(0.1) # Short delay
 
-    button = []
-    if len(button) == 5:
-        if sum(button) == 5:
-            print("Payload on")
-        else: 
-            print("No Payload")
-        
-        button = []
-
-
+'''
 from machine import ADC
 from time import sleep
 
@@ -139,7 +132,7 @@ while True:
     print(ir.read_u16())
     
     sleep(0.1)
-
+'''
 
 
 
